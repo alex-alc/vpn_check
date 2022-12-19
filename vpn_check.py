0032e66b6b6e0cf353d6cce4ee7603b504d57a51
Alex AL <43143903+alex-alc@users.noreply.github.com>
2022-12-19 04:00:44 +0200

    bootstrap correction

--- vpn_check.py
+++ vpn_check.py
@@ -13,7 +13,7 @@ server = app.server
 
 app.layout = (html.Div(
 	className="data-gr-ext-installed style class", children=[
-		html.Div(className="col-lg-6 offset-lg-3 col-md-4 offset-md-4 col-sm-4 offset-sm-4", style={"textAlign": "center"}, children=[
+		html.Div(className="col-6 offset-3", style={"textAlign": "center"}, children=[
 			html.Br(),
 			html.H1("Beskyd VPN Checking"),
 			html.Hr(),
