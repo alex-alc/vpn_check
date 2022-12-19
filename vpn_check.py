2502e1438ca6e035b29b328e474bade7693766fd
Alex AL <43143903+alex-alc@users.noreply.github.com>
2022-12-19 03:44:05 +0200

    bootstrap correction

--- vpn_check.py
+++ vpn_check.py
@@ -3,86 +3,77 @@ from dash.dcc import Interval
 from dash.dependencies import Input, Output
 from datetime import datetime, timedelta
 import dash_bootstrap_components as dbc
-# from loguru import logger
+from loguru import logger
 
-# from messenger import notification
 from telnet_func import telnet_connection
 
 dbc_css = r'assets/bootstrap.min.css'
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
 server = app.server
 
-header = html.H4("Beskyd VPN Checking", className="card text-white bg-success mb-3", style="max-width: 20rem;")
-
-app.layout = html.Div(className="data-gr-ext-installed style class", children=[
-
-	html.Div(className="col-lg-8 offset-lg-2 col-md-6 offset-md-3 col-sm-6 offset-md-3", style={"textAlign": "center"}, children=[
-		html.Br(),
-		html.H1("Beskyd VPN Checking"),
-		html.Hr(),
-		html.Br(),
-		html.Div(id='output_div'),
-	]),
-
-	Interval(
-		id='interval_component',
-		max_intervals=-1,
-		interval=1000 * 60 * 15,  # in milliseconds, 1000*60*15 = 15 mins.
-		n_intervals=0,
-	),
-])
+app.layout = (html.Div(
+	className="data-gr-ext-installed style class", children=[
+		html.Div(className="col-lg-6 offset-lg-3 col-md-4 offset-md-4 col-sm-4 offset-md-4", style={"textAlign": "center"}, children=[
+			html.Br(),
+			html.H1("Beskyd VPN Checking"),
+			html.Hr(),
+			html.Br(),
+			html.Div(id='output_div'),
+		]),
+		Interval(
+			id='interval_component',
+			max_intervals=-1,
+			interval=1000 * 60 * 15,  # in milliseconds: 1000 * 60 * 15 = 15 mins.
+			n_intervals=0
+		)
+	])
+)
 
 
 @app.callback(Output('output_div', 'children'),
               Input('interval_component', 'n_intervals'))
 def response_output(value):
+	result = None
 	try:
 		result = telnet_connection()
 	except Exception as ex:
-		# logger.critical(ex)
+		logger.critical(ex)
 		pass
 	time_ = datetime.now()
 	t = time_ + timedelta(minutes=120)
-	tt = t.strftime("%H:%M:%S")
 	last_update = t.strftime("%d.%m.%Y %H:%M:%S")
 
 	if result == "Success":
-		return html.Div(
-			id='output_div', className="card text-white bg-success mb-3", style="max-width: 20rem;", children=[
+		return html.Div(id='output_div', children=[
 				html.Div(className="card-header", children=["Connection to the VPN"]),
 				html.Div(className="card-body", children=[
 					html.H4(className="card-title", children=["Everything works fine!"]),
-					html.P(className="card-text", children=[
-						f"Last update: {last_update}",
-						html.Img(src=r'assets/done.png', width=100, alt="Success"),  # Success Img
-					]),
-				]),
-			]),
+					html.P(className="card-text", children=[f"Last update: {last_update}"]),
+					html.Img(src=r'assets/done.png', width=100, alt="Success"),
+					html.Br()
+				])
+		], className="card text-white bg-success mb-3", style={"max-width": "20rem;"})
+
 	elif result == "TimeoutError":
-		return html.Div(
-			id='output_div', className="card text-white bg-danger mb-3", style="max-width: 20rem;", children=[
-				html.Div(className="card-header", children=["Connection to the VPN"]),  # Card Header
-				html.Div(className="card-body", children=[  # Card Body
+		return html.Div(id='output_div', children=[
+				html.Div(className="card-header", children=["Connection to the VPN"]),
+				html.Div(className="card-body", children=[
 					html.H4(className="card-title", children=["The connection to the VPN is lost!"]),
-					html.P(className="card-text", children=[
-						f"Last update: {last_update}",
-						html.Img(src=r'assets/error.png', width=100, alt="Error"),  # Error Img
-					]),
-				]),
-			]),
+					html.P(className="card-text", children=[f"Last update: {last_update}"]),
+					html.Img(src=r'assets/error.png', width=100, alt="Error"),
+					html.Br()
+				])
+			], className="card text-white bg-danger mb-3", style={"max-width": "20rem;"})
 	else:
-		return html.Div(
-			id='output_div', className="card text-white bg-danger mb-3", style="max-width: 20rem;", children=[
-				html.Div(className="card-header", children=["Connection to the VPN"]),  # Card Header
-				html.Div(className="card-body", children=[  # Card Body
+		return html.Div(id='output_div', children=[
+				html.Div(className="card-header", children=["Connection to the VPN"]),
+				html.Div(className="card-body", children=[
 					html.H4(className="card-title", children=["Something is wrong..."]),
-					# DANGER card title
-					html.P(className="card-text", children=[
-						f"Last update: {last_update}",
-						html.Img(src=r'assets/error.png', width=100, alt="Error"),  # Error Img
-					]),
-				]),
-			]),
+					html.P(className="card-text", children=[f"Last update: {last_update}"]),
+					html.Img(src=r'assets/error.png', width=100, alt="Error"),
+					html.Br()
+				])
+			], className="card text-white bg-danger mb-3", style = {"max-width": "20rem;"})
 
 
 if __name__ == '__main__':
