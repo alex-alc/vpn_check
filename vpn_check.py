f27187651e029d55d25e8eef652540156dc5a727
Alex AL <43143903+alex-alc@users.noreply.github.com>
2022-12-19 02:11:59 +0200

    bootstrap and external_stylesheets implemented

--- vpn_check.py
+++ vpn_check.py
@@ -2,113 +2,88 @@ from dash import html, Dash
 from dash.dcc import Interval
 from dash.dependencies import Input, Output
 from datetime import datetime, timedelta
-import pytz
+import dash_bootstrap_components as dbc
 # from loguru import logger
 
 # from messenger import notification
 from telnet_func import telnet_connection
 
-
-app = Dash(__name__)
+dbc_css = r'assets/bootstrap.min.css'
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
 server = app.server
 
-app.layout = html.Div([
-    html.Title("Beskyd VPN Checking"),
-    html.Br(),
-    html.H1('Beskyd VPN Checking'),
-    html.Hr(),
-    html.Br(),
-    html.H2('Connection to the VPN'),
-    html.Br(),
-    html.Div(id='output_div'),
-    Interval(
-        id='interval_component',
-        max_intervals=-1,
-        interval=1000*60*15,  # in milliseconds, 1000*60*15 = 15 mins.
-        n_intervals=0,
-    ),
-    ],
-    style={
-        'margin-left': 30,
-        'margin-right': 100,
-    })
+header = html.H4("Beskyd VPN Checking", className="card text-white bg-success mb-3", style="max-width: 20rem;")
+
+app.layout = html.Div(className="data-gr-ext-installed style class", children=[
+	html.Div(className="col-lg-8 offset-lg-2 col-md-6 offset-md-3 col-sm-6 offset-md-3", style={"textAlign": "center"}, children=[
+		html.Br(),
+		html.H1("Beskyd VPN Checking"),
+		html.Hr(),
+		html.Br(),
+	]),
+
+	html.Div(id='output_div'),
+
+	Interval(
+		id='interval_component',
+		max_intervals=-1,
+		interval=1000 * 60 * 15,  # in milliseconds, 1000*60*15 = 15 mins.
+		n_intervals=0,
+	),
+])
 
 
 @app.callback(Output('output_div', 'children'),
               Input('interval_component', 'n_intervals'))
 def response_output(value):
-    result: str = ""
-    try:
-        result = telnet_connection()
-    except Exception as ex:
-        # logger.critical(ex)
-        pass
-    time_ = datetime.now()
-    t = time_ + timedelta(minutes=120)
-    tt = t.strftime("%H:%M:%S")
-    last_update = t.strftime("%d.%m.%Y %H:%M:%S")
-    sending_h = int(t.strftime("%H"))
-    sending_mins = t + timedelta(minutes=1)
-    sending_m = int(sending_mins.strftime("%M"))
+	try:
+		result = telnet_connection()
+	except Exception as ex:
+		# logger.critical(ex)
+		pass
+	time_ = datetime.now()
+	t = time_ + timedelta(minutes=120)
+	tt = t.strftime("%H:%M:%S")
+	last_update = t.strftime("%d.%m.%Y %H:%M:%S")
 
-    if result == "Success":
-        # msg = f"{last_update} - VPN works fine!"
-        # try:
-        #     notification(msg, sending_h, sending_m)
-        # except Exception as ex:
-        #     logger.warning(ex)
-        #     pass
-        return html.Div(
-                style={'color': '#145A32'},
-                children=[
-                    html.H3("Success"),
-                    html.Br(),
-                    html.Img(src=r'assets/done.png', width=100, alt="Success"),
-                    html.Br(),
-                    html.Br(),
-                    html.H4("Everything works fine!"),
-                    html.Br(),
-                    html.H4(f'Last update: {last_update}'),
-                    html.Br(),
-                ])
-    elif result == "TimeoutError":
-        # msg = f"{last_update} - TimeoutError - The connection to the VPN is not working!"
-        # try:
-        #     notification(msg, sending_h, sending_m)
-        # except Exception as ex:
-        #     logger.warning(ex)
-        #     pass
-        return html.Div(
-            style={'color': 'red'},
-            children=[
-                html.H3("Timeout Error"),
-                html.Br(),
-                html.Img(src=app.get_asset_url('error.png'), width=100, alt="Error"),
-                html.Br(),
-                html.Br(),
-                html.H4("The connection to the VPN is not working!"),
-                html.Br(),
-                html.H4(f'Last update: {last_update}'),
-                html.Br(),
-            ])
-    else:
-        # msg = f"{last_update} - OtherException - Something is wrong!"
-        # try:
-        #     notification(msg, sending_h, sending_m)
-        # except Exception as ex:
-        #     logger.warning(ex)
-        #     pass
-        return html.Div(
-            style={'color': 'red'},
-            children=[
-                html.H3("Something is wrong..."),
-                html.Br(),
-                html.H4("Ping function doesn't work!"),
-                html.Br(),
-                html.H4(f'Last update: {last_update}'),
-                html.Br(),
-            ])
+	if result == "Success":
+		return html.Div(
+			id='output_div', className="card text-white bg-success mb-3", style="max-width: 20rem;", children=[
+				html.Div(className="card-header", children=["Connection to the VPN"]),
+				html.Div(className="card-body", children=[
+					html.H4(className="card-title", children=["Everything works fine!"]),
+					html.P(className="card-text", children=[
+						f"Last update: {last_update}",
+						html.Img(src=r'assets/done.png', width=100, alt="Success"),  # Success Img
+					]),
+				]),
+			]),
+	elif result == "TimeoutError":
+		return html.Div(
+			id='output_div', className="card text-white bg-danger mb-3", style="max-width: 20rem;", children=[
+				html.Div(className="card-header", children=["Connection to the VPN"]),  # Card Header
+				html.Div(className="card-body", children=[  # Card Body
+					html.H4(className="card-title", children=["The connection to the VPN is lost!"]),
+					html.P(className="card-text", children=[
+						f"Last update: {last_update}",
+						html.Img(src=r'assets/error.png', width=100, alt="Error"),  # Error Img
+					]),
+				]),
+			]),
+	else:
+		return html.Div(
+			id='output_div', className="card text-white bg-danger mb-3", style="max-width: 20rem;", children=[
+				html.Div(className="card-header", children=["Connection to the VPN"]),  # Card Header
+				html.Div(className="card-body", children=[  # Card Body
+					html.H4(className="card-title", children=["Something is wrong..."]),
+					# DANGER card title
+					html.P(className="card-text", children=[
+						f"Last update: {last_update}",
+						html.Img(src=r'assets/error.png', width=100, alt="Error"),  # Error Img
+					]),
+				]),
+			]),
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+	app.run(debug=False)
